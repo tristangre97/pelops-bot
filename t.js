b0da23403fc8adfc0863ad4ef77b30b56ea77756
Tristan <tristangre97@gmail.com>
2022-05-29 18:37:52 -0400

    Removed old version of bot

--- t.js
+++ t.js
@@ -1,3 +1,48 @@
-const db = require('./utility/database');
-// db.set(`stats.uses`, 4605)
-console.log(db.get(`stats.uses`));
+const cache = require('./utility/cache.js');
+const fs = require('fs');
+sTier = []
+aTier = []
+bTier = []
+cTier = []
+dTier = []
+async function updateTierList() {
+
+    unitData = JSON.parse(fs.readFileSync(`/home/tristan/Downloads/pelops/data/unitData.json`, 'utf8'));
+    unitData.forEach(unit => {
+        unitTier = unit['TIER']
+        console.log(unitTier)
+        if (unitTier == "S") {
+            console.log("S tier")
+            sTier.push(unit['Unit Name'])
+        }
+        if (unitTier == "A") {
+            aTier.push(unit['Unit Name'])
+        }
+        if (unitTier == "B") {
+            bTier.push(unit['Unit Name'])
+        }
+        if (unitTier == "C") {
+            cTier.push(unit['Unit Name'])
+        }
+        if (unitTier == "D") {
+            dTier.push(unit['Unit Name'])
+        }
+    })
+    console.log('Done')
+    tierList = {
+        "S": sTier,
+        "A": aTier,
+        "B": bTier,
+        "C": cTier,
+        "D": dTier,
+
+    }
+    console.log(tierList)
+    // cache.set("tierList", tierList, 0);
+}
+
+
+updateTierList()
+
+
+// console.log(cache.get("tierList"))
