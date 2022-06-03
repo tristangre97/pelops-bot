53a4a6caee6dcb9b60480948ad38fe78192cef86
Tristan <tristangre97@gmail.com>
2022-06-02 23:45:55 -0400

    Testing junk

--- t.js
+++ t.js
@@ -1,48 +1,61 @@
-const cache = require('./utility/cache.js');
-const fs = require('fs');
-sTier = []
-aTier = []
-bTier = []
-cTier = []
-dTier = []
-async function updateTierList() {
+const search = require('./utility/search')
+const unitEmbedGen = require('./utility/getUnitData.js');
 
-    unitData = JSON.parse(fs.readFileSync(`/home/tristan/Downloads/pelops/data/unitData.json`, 'utf8'));
-    unitData.forEach(unit => {
-        unitTier = unit['TIER']
-        console.log(unitTier)
-        if (unitTier == "S") {
-            console.log("S tier")
-            sTier.push(unit['Unit Name'])
-        }
-        if (unitTier == "A") {
-            aTier.push(unit['Unit Name'])
-        }
-        if (unitTier == "B") {
-            bTier.push(unit['Unit Name'])
-        }
-        if (unitTier == "C") {
-            cTier.push(unit['Unit Name'])
-        }
-        if (unitTier == "D") {
-            dTier.push(unit['Unit Name'])
-        }
-    })
-    console.log('Done')
-    tierList = {
-        "S": sTier,
-        "A": aTier,
-        "B": bTier,
-        "C": cTier,
-        "D": dTier,
+var seasonOptions = []
 
+i = 0;
+
+while(i < 14) {
+    var data = {
+        name: `${i}`,
+        value: `${i}`,
     }
-    console.log(tierList)
-    // cache.set("tierList", tierList, 0);
+    seasonOptions.push(data)
+    i++
 }
 
 
-updateTierList()
+return console.log(seasonOptions)
 
 
-// console.log(cache.get("tierList"))
+async  function unit() {
+    searchResults = await search.unitSearch(' Shin Godzilla (Stage 2)');
+unit = searchResults[0].item;
+
+embed = await unitEmbedGen.getUnitEmbed(unit, 30)
+
+console.log(embed)
+
+}
+
+
+unit()
+
+
+
+
+
+var updateList = [{
+    name: "unitData",
+    fullName: "Unit Data",
+    type: "download",
+    url: "https://sheetsu.com/apis/v1.0su/bfb7ac95068b",
+},
+{
+    name: "mapLogs",
+    fullName: "Map Logs",
+    type: "download",
+    url: "https://sheetsu.com/apis/v1.0bu/9acebc3f7c89",
+},
+{
+    name: "autocomplete",
+    fullName: "Autocomplete",
+    type: "cache",
+},
+{
+    name: "tierList",
+    fullName: "Tier List",
+    type: "download",
+    url: "https://sheetsu.com/apis/v1.0bu/9acebc3f7c89",
+}
+];
