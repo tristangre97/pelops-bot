31f1749be1277fa42597015d58e517a380d79634
Tristan <tristangre97@gmail.com>
2022-06-05 00:54:14 -0400

    File name change

--- pelops.js
+++ pelops.js
@@ -52,7 +52,7 @@ client.on('ready', () => {
   });
   cache.set('unitData', fs.readFileSync('/home/tristan/Downloads/pelops/data/unitData.json', 'utf8'), 0);
   cache.set('mapLogs', fs.readFileSync('/home/tristan/Downloads/pelops/data/mapLogs.json', 'utf8'), 0);
-  cache.set('seasonListData', fs.readFileSync('/home/tristan/Downloads/pelops/data/seasonData.json', 'utf8'), 0);
+  cache.set('seasonList', fs.readFileSync('/home/tristan/Downloads/pelops/data/seasonData.json', 'utf8'), 0);
   cache.set("pelops_update_status", "finished", 0);
   updateUnitNameList()
   console.log("bot is up!");
