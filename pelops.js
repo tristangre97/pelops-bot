387697cd91b6cb4b39377b3d50c7e0b82c9b6631
Tristan <tristangre97@gmail.com>
2022-06-06 22:48:48 -0400

    bot will now tell the user when an error occured

--- pelops.js
+++ pelops.js
@@ -3,6 +3,11 @@ const path = require("path");
 const {
   Intents
 } = DiscordJS;
+const {
+  MessageEmbed,
+  MessageActionRow,
+  MessageButton
+} = require('discord.js');
 const mongoose = require("mongoose");
 const config = require("./auth.json");
 const cache = require('./utility/cache.js');
@@ -41,7 +46,7 @@ client.on('ready', () => {
     slashGuilds: ["682387138763161630"],
     allSlash: true,
     owners: ["222781123875307521"],
-    handleSlash: true,
+    handleSlash: 'both',
     handleNormal: false,
     prefix: ",", // Bot's prefix
     timeout: false, // If you want to add timeouts in commands
@@ -52,10 +57,58 @@ client.on('ready', () => {
   });
   cache.set('unitData', fs.readFileSync('/home/tristan/Downloads/pelops/data/unitData.json', 'utf8'), 0);
   cache.set('mapLogs', fs.readFileSync('/home/tristan/Downloads/pelops/data/mapLogs.json', 'utf8'), 0);
-  cache.set('seasonList', fs.readFileSync('/home/tristan/Downloads/pelops/data/seasonData.json', 'utf8'), 0);
+  cache.set('seasonData', fs.readFileSync('/home/tristan/Downloads/pelops/data/seasonData.json', 'utf8'), 0);
   cache.set("pelops_update_status", "finished", 0);
   updateUnitNameList()
   console.log("bot is up!");
+
+
+
+
+
+  handler.on('slashCommand', async (command, command_data) => {
+
+    if (command.adminOnly === true) {
+      if (!command_data.member.permissions.has("ADMINISTRATOR")) {
+        command_data.message.editReply("You must be an admin to use this command!");
+        return
+      }
+    }
+
+
+    try {
+      await command.run(command_data);
+      var date = new Date(Date.now());
+      console.log(`${command_data.user.username} used ${command.name} in ${command_data.guild.name} (${date.toLocaleString()})`);
+
+    } catch (e) {
+      console.error(e);
+      errorMessageEmbed = new MessageEmbed()
+      errorMessageEmbed.setColor('#ba1b1b');
+      errorMessageEmbed.setTitle(`Error`);
+      errorMessageEmbed.setDescription(`An error occured while running the command: \`${command.name}\`\n\n**Error Message**: \`${e.message}\``);
+       row = new MessageActionRow();
+      // originalUser = interaction.user.id;
+      row.addComponents(
+        new MessageButton()
+        .setLabel(`Support Server`)
+        .setStyle('LINK')
+        .setURL(`https://dsc.gg/tristangames`)
+      )
+
+      command_data.message.editReply({
+        embeds: [errorMessageEmbed],
+        components: [row]
+      });
+
+    }
+  })
+
+
+
+
+
+
 });
 
 client.login(config.token);
