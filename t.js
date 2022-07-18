45bd8349549cfb3c4ba5e05d962f17b788b46696
Tristan <tristangre97@gmail.com>
2022-07-18 18:23:07 -0400

    Added t.js

--- t.js
+++ t.js
@@ -1,26 +1,9 @@
-const search = require('./utility/search')
-const mathjs = require('mathjs')
+const html2img = require('./utility/HTML2IMG');
 
-var results = search.starRankSearch('Hedorah')
-return console.log(results)
-totalHPBonus = 0;
-totalDmgBonus = 0
+html = `
+<b>Hello World</b>
+`
 
-star_rank = 5
-i = 1
-for (item in results) {
-    bonusType = results[item]
-    if (i >= star_rank) {
-    } else {
-        if (bonusType.startsWith('HP')) {
-            totalHPBonus = Math.abs(totalHPBonus + Number(bonusType.split('+')[1].trim().replace('%', '')))
-        }
-        if (bonusType.startsWith('Dmg')) {
-            totalDmgBonus = Math.abs(totalDmgBonus + Number(bonusType.split('+')[1].trim().replace('%', '')))
-        }
-    }
-    i++
-}
-console.log(totalHPBonus)
-console.log(totalDmgBonus)
 
+
+console.log(html2img.makeTest(html, 'body'))
