bc5ceaea4cdf1cf36825b08f6f2bfba49385dd57
Tristan Gregory <tristangre97@gmail.com>
2022-12-04 20:44:58 -0500

    Started on unitEmbedGenV2

--- utility/getUnitDataV2.js
+++ utility/getUnitDataV2.js
@@ -1,6 +1,36 @@
-const unitData = require('../data/unitData.json');
-// unitData to a map
+const {
+    EmbedBuilder,
+    ActionRowBuilder,
+    ButtonBuilder
+} = require('discord.js');
+
+const unitDataFile = require('../data/unitData.json');
 const unitDataMap = new Map();
-unitData.forEach((unit) => {
-    unitDataMap.set(unit.name, unit);
-    });
+unitDataFile.forEach((unit) => {
+    unitDataMap.set(unit['Unit Name'], unit);
+});
+
+
+
+
+
+exports.get = async function (data) {
+
+    var {id, user, time, guild, unit: {name, level, star_rank, apply_boost}} = data;
+
+    const embed = new EmbedBuilder()
+    const components = []
+
+    unitData = unitDataMap.get(name);
+
+    if(!unitData) {
+        embed.setTitle(`Unit not found`)
+        embed.setDescription(`Unit ${name} not found`)
+        return {
+            embed: embed,
+            components: components
+        }
+    }
+
+    console.log(unitData)
+}
