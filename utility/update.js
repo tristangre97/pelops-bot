8989297a4495324938ea0780d50688afd52b4156
Tristan Gregory <tristangre97@gmail.com>
2023-09-17 13:18:43 -0400

    New stuff

--- utility/update.js
+++ utility/update.js
@@ -1,8 +1,12 @@
-const cache = require("./cache.js");
-const fetch = require("node-fetch");
-const humanizeDuration = require("humanize-duration");
+const auth = require("../auth.json");
+const authorizedUsers = auth.authorizedUsers;
 
-dataFiles = [
+const cache = require("./cache.js");
+const convert = require("./convert");
+const fetch = require("node-fetch");
+const fs = require("node:fs/promises");
+
+const files = [
   {
     name: "unitData",
     fullName: "Unit Data",
@@ -12,16 +16,19 @@ dataFiles = [
     name: "mapLogs",
     fullName: "Map Logs",
     url: "https://sheetsu.com/apis/v1.0bu/9acebc3f7c89",
+    skip: true,
   },
   {
     name: "seasonData",
     fullName: "Season List Data",
     url: "https://sheetsu.com/apis/v1.0bu/b5f4fd1de48b",
+    skip: true,
   },
   {
     name: "leaderData",
     fullName: "Unit Leader Data",
     url: "https://sheetsu.com/apis/v1.0su/9c52e24e16f7",
+    skip: true,
   },
   {
     name: "starRankRewards",
@@ -30,16 +37,46 @@ dataFiles = [
   }
 ]
 
-// turn dataFiles into a map
-var dataMap = new Map();
-dataFiles.forEach((data) => {
-  dataMap.set(data.name, data);
-});
+const updateFiles = files.filter(file => !file.skip);
 
-console.log(dataMap);
 
-exports.update = function (name) {
 
-  
+exports.update = async function (interaction) {
+
+
+  await interaction.editReply({
+    content: `Updating ${updateFiles.length} files...`,
+  })
+
+  const successFiles = [];
+  const failedFiles = [];
+  start = performance.now();
+  for (item of updateFiles) {
+    try {
+      const response = await fetch(item.url);
+      const data = await response.json();
+      if (!response.status == 200) return;
+
+      await fs.writeFile(`./data/${item.name}.json`, JSON.stringify(data))
+
+      cache.set(item.name, JSON.stringify(data), 0);
+      console.log(`Updated ${item.fullName} [${item.name}]`);
+      successFiles.push(item.fullName);
+      await interaction.editReply({
+        content: `Updated ${successFiles.join(",")}`,
+      })
+    } catch (error) {
+      console.log(`Error updating ${item.fullName}`);
+      console.log(error);
+      failedFiles.push(item.fullName);
+    }
+  }
+  end = performance.now();
+
+  return {
+    successFiles,
+    failedFiles,
+    time: (end - start).toFixed(2),
+  }
 
 };
